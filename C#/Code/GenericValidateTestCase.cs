b101c0248fef615f7dbffe8eae645f130a008b90
Jeffrey Hoover <jeffreyhoover1@gmail.com>
2023-02-13 18:12:01 -0500

    fix validate bug

--- C#/Code/GenericValidateTestCase.cs
+++ C#/Code/GenericValidateTestCase.cs
@@ -5,21 +5,21 @@ namespace C_Sharp;
 
 public class GenericValidateTestCase<T> : IXunitSerializable
 {
-    public string TestName { get; set; }
+    public string _TestName { get; set; }
     public T TestObject { get; set; }
     public Dictionary<string, object> Metadata { get; set; }
 
     public void Deserialize(IXunitSerializationInfo info)
     {
-        TestName = info.GetValue<string>(nameof(TestName));
-        TestObject = JsonConvert.DeserializeObject<T>(info.GetValue<string>(nameof(TestName)));
+        _TestName = info.GetValue<string>(nameof(_TestName));
+        TestObject = JsonConvert.DeserializeObject<T>(info.GetValue<string>(nameof(_TestName)));
         Metadata = JsonConvert.DeserializeObject<Dictionary<string, object>>(info.GetValue<string>(nameof(Metadata)));
     }
 
     public void Serialize(IXunitSerializationInfo info)
     {
-        info.AddValue(nameof(TestName), TestName);
-        info.AddValue(nameof(TestObject), TestObject == null ? Activator.CreateInstance<T> : JsonConvert.SerializeObject(TestObject));
-        info.AddValue(nameof(Metadata), Metadata == null ? new Dictionary<string, object>() : JsonConvert.SerializeObject(Metadata));
+        info.AddValue(nameof(_TestName), _TestName);
+        info.AddValue(nameof(TestObject), TestObject == null ? JsonConvert.SerializeObject(Activator.CreateInstance<T>()) : JsonConvert.SerializeObject(TestObject));
+        info.AddValue(nameof(Metadata), Metadata == null ? JsonConvert.SerializeObject(new Dictionary<string, object>()) : JsonConvert.SerializeObject(Metadata));
     }
 }
